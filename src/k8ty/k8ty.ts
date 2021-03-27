43a3fd95a35184d74f93e076fad3659ab0847448
Mark Rudolph <mark@k8ty.app>
2021-03-26 20:47:18 -0400

    modularized some of the calls

--- src/k8ty/k8ty.ts
+++ src/k8ty/k8ty.ts
@@ -9,6 +9,169 @@ export namespace K8ty {
   export const appClient = kc.makeApiClient(k8s.AppsV1Api)
   export const networkClient = kc.makeApiClient(k8s.NetworkingV1beta1Api)
 
+  const createNamespace =  (name: string) => coreClient.createNamespace({
+    metadata: {
+      name: name,
+      labels: {
+        'k8ty.app': 'true',
+        'k8ty.app/name': name,
+      },
+    },
+  })
+
+  const createSecret = (name: string) => coreClient.createNamespacedSecret(name, {
+    metadata: {
+      name: name,
+      labels: {
+        'k8ty.app': 'true',
+        'k8ty.app/name': name,
+      },
+    },
+  })
+
+  const createDeployment = (name: string) => appClient.createNamespacedDeployment(name, {
+    metadata: {
+      name: name,
+      labels: {
+        'k8ty.app': 'true',
+        'k8ty.app/name': name,
+        app: name,
+      },
+    },
+    spec: {
+      replicas: 1,
+      selector: {
+        matchLabels: {
+          app: name,
+        },
+      },
+      template: {
+        metadata: {
+          labels: {
+            app: name,
+          },
+        },
+        spec: {
+          containers: [
+            {
+              name: name,
+              image: 'nginx',
+              imagePullPolicy: 'Always',
+              stdin: true,
+              tty: true,
+              envFrom: [
+                {
+                  secretRef: {
+                    name: name,
+                  },
+                },
+              ],
+              ports: [
+                {
+                  name: 'http',
+                  containerPort: 80,
+                  protocol: 'TCP',
+                },
+              ],
+              livenessProbe: {
+                initialDelaySeconds: 10,
+                periodSeconds: 30,
+                httpGet: {
+                  path: '/',
+                  port: 'http' as unknown as object,
+                },
+              },
+              readinessProbe: {
+                initialDelaySeconds: 10,
+                periodSeconds: 30,
+                httpGet: {
+                  path: '/',
+                  port: 'http' as unknown as object,
+                },
+              },
+              resources: {
+                limits: {
+                  cpu: '500m',
+                  memory: '1Gi',
+                },
+                requests: {
+                  cpu: '250m',
+                  memory: '250Mi',
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+  })
+
+  const createService = (name: string) => coreClient.createNamespacedService(name, {
+    metadata: {
+      name: name,
+      labels: {
+        'k8ty.app': 'true',
+        'k8ty.app/name': name,
+        app: name,
+      },
+    },
+    spec: {
+      type: 'ClusterIP',
+      selector: {
+        app: name,
+      },
+      ports: [
+        {
+          port: 80,
+          name: 'http',
+          protocol: 'TCP',
+          targetPort: 'http' as unknown as object,
+        },
+      ],
+    },
+  })
+
+  const createIngress = (name: string) => networkClient.createNamespacedIngress(name, {
+    metadata: {
+      name: name,
+      labels: {
+        'k8ty.app': 'true',
+        'k8ty.app/name': name,
+      },
+      annotations: {
+        'kubernetes.io/ingress.class': 'nginx',
+        'cert-manager.io/cluster-issuer': 'letsencrypt',
+      },
+    },
+    spec: {
+      tls: [
+        {
+          hosts: [`${name}.k8ty.app`],
+          secretName: `${name}-tls`,
+        },
+      ],
+      rules: [
+        {
+          host: `${name}.k8ty.app`,
+          http: {
+            paths: [
+              {
+                path: '/',
+                backend: {
+                  serviceName: name,
+                  servicePort: 'http' as unknown as object,
+                },
+              },
+            ],
+          },
+        },
+      ],
+    },
+  })
+
+  // const createK8tyrc = coreClient.createNamespacedConfigMap('', {})
+  // const persistK8tyrc = Promise.resolve(false)
+
   export interface ListAppResponse {
     response: http.IncomingMessage;
     body: V1NamespaceList;
@@ -29,180 +192,22 @@ export namespace K8ty {
 
   export const createApp = async (name: string) => {
     // Create a Namespace
-    coreClient.createNamespace({
-      metadata: {
-        name: name,
-        labels: {
-          'k8ty.app': 'true',
-          'k8ty.app/name': name,
-        },
-      },
-    })
+    createNamespace(name)
     .then(_ => {
       // Create a secret
-      return coreClient.createNamespacedSecret(name, {
-        metadata: {
-          name: name,
-          labels: {
-            'k8ty.app': 'true',
-            'k8ty.app/name': name,
-          },
-        },
-      })
+      return createSecret(name)
     })
     .then(_ => {
       // Create a deployment
-      return appClient.createNamespacedDeployment(name, {
-        metadata: {
-          name: name,
-          labels: {
-            'k8ty.app': 'true',
-            'k8ty.app/name': name,
-            app: name,
-          },
-        },
-        spec: {
-          replicas: 1,
-          selector: {
-            matchLabels: {
-              app: name,
-            },
-          },
-          template: {
-            metadata: {
-              labels: {
-                app: name,
-              },
-            },
-            spec: {
-              containers: [
-                {
-                  name: name,
-                  image: 'nginx',
-                  imagePullPolicy: 'Always',
-                  stdin: true,
-                  tty: true,
-                  envFrom: [
-                    {
-                      secretRef: {
-                        name: name,
-                      },
-                    },
-                  ],
-                  ports: [
-                    {
-                      name: 'http',
-                      containerPort: 80,
-                      protocol: 'TCP',
-                    },
-                  ],
-                  livenessProbe: {
-                    initialDelaySeconds: 10,
-                    periodSeconds: 30,
-                    httpGet: {
-                      path: '/',
-                      port: 'http' as unknown as object,
-                    },
-                  },
-                  readinessProbe: {
-                    initialDelaySeconds: 10,
-                    periodSeconds: 30,
-                    httpGet: {
-                      path: '/',
-                      port: 'http' as unknown as object,
-                    },
-                  },
-                  resources: {
-                    limits: {
-                      cpu: '500m',
-                      memory: '1Gi',
-                    },
-                    requests: {
-                      cpu: '250m',
-                      memory: '250Mi',
-                    },
-                  },
-                },
-              ],
-            },
-          },
-        },
-      })
+      return createDeployment(name)
     })
     .then(_ => {
-      return coreClient.createNamespacedService(name, {
-        metadata: {
-          name: name,
-          labels: {
-            'k8ty.app': 'true',
-            'k8ty.app/name': name,
-            app: name,
-          },
-        },
-        spec: {
-          type: 'ClusterIP',
-          selector: {
-            app: name,
-          },
-          ports: [
-            {
-              port: 80,
-              name: 'http',
-              protocol: 'TCP',
-              targetPort: 'http' as unknown as object,
-            },
-          ],
-        },
-      })
+      return createService(name)
     })
     .then(_ => {
-      return networkClient.createNamespacedIngress(name, {
-        metadata: {
-          name: name,
-          labels: {
-            'k8ty.app': 'true',
-            'k8ty.app/name': name,
-          },
-          annotations: {
-            'kubernetes.io/ingress.class': 'nginx',
-            'cert-manager.io/cluster-issuer': 'letsencrypt',
-          },
-        },
-        spec: {
-          tls: [
-            {
-              hosts: [`${name}.k8ty.app`],
-              secretName: `${name}-tls`,
-            },
-          ],
-          rules: [
-            {
-              host: `${name}.k8ty.app`,
-              http: {
-                paths: [
-                  {
-                    path: '/',
-                    backend: {
-                      serviceName: name,
-                      servicePort: 'http' as unknown as object,
-                    },
-                  },
-                ],
-              },
-            },
-          ],
-        },
-      })
+      return createIngress(name)
     })
   }
-
-  // const createNamespace = coreClient.createNamespace({})
-  // const createSecret = coreClient.createNamespacedSecret('', {})
-  // const createDeployment = appClient.createNamespacedDeployment('', {})
-  // const createService = coreClient.createNamespacedService('', {})
-  // create ingress!
-  // const createK8tyrc = coreClient.createNamespacedConfigMap('', {})
-  // const persistK8tyrc = Promise.resolve(false)
 
   export interface ReadAppResponse {
     response: http.IncomingMessage;
