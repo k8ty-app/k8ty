c2bb1eaf8ef1b687ee1ce0a45f4270260c354fdf
Mark Rudolph <mark@k8ty.app>
2021-03-27 10:29:02 -0400

    restructure some code

--- src/commands/app/delete.ts
+++ src/commands/app/delete.ts
@@ -1,6 +1,5 @@
 import Command from '@oclif/command'
 import {K8ty} from '../../k8ty/k8ty'
-import * as util from 'util'
 
 export default class DeleteApp extends Command {
  static description = 'Delete an app'
@@ -10,24 +9,24 @@ export default class DeleteApp extends Command {
   async run() {
     const {args} = this.parse(DeleteApp)
     if (args.name) {
-      const canDelete = await K8ty.coreClient.readNamespace(args.name)
-      .then(res => res.body.metadata?.labels?.['k8ty.app'] === 'true')
-      .catch(error => {
-        this.error(`k8ty.app ${args.name} doesn't seem to exist!`)
-        this.log(util.inspect(error))
-        return false
+      await K8ty.coreClient.readNamespace(args.name)
+      .then(res =>  {
+        return res.body.metadata?.labels?.['k8ty.app'] === 'true'
       })
-
-      if (canDelete) {
-        await K8ty.coreClient.deleteNamespace(args.name)
-        .then(_ => this.log(`k8ty.app ${args.name} deleted!`))
-        .catch(error => {
-          this.error(`Unable to delete ${args.name}`)
-          this.log(util.inspect(error))
-        })
-      } else {
-        this.error(`${args.name} exists, but is not a k8ty.app! Won't delete...`)
-      }
+      .then(async canDelete => {
+        if (canDelete) {
+          await K8ty.coreClient.deleteNamespace(args.name)
+          .then(_ => this.log(`k8ty.app ${args.name} deleted!`))
+          .catch(_ => {
+            this.log(`Unable to delete ${args.name}`)
+          })
+        } else {
+          this.log(`${args.name} exists, but is not a k8ty.app! Won't delete...`)
+        }
+      })
+      .catch(_ => {
+        this.log(`k8ty.app ${args.name} doesn't seem to exist!`)
+      })
     } else {
       this._help()
     }
